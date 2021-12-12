da40ec04fdc8b45ca9c2718b76977979845e63cb
flytreeleft <flytreeleft@crazydan.org>
2021-12-13 00:18:24 +0800

    Define HDML.Block

--- src/HDML/Block.elm
+++ src/HDML/Block.elm
@@ -1,3 +1,20 @@
-module HDML.Block exposing (..)
+module HDML.Block exposing (Block(..), Attr(..))
+
+import Dict exposing (Dict)
 
 
+type Block a =
+    Named String (AttrChild a) (Blocks a)
+    | Anonymous (AttrChild a) (Blocks a)
+    | Text (AttrChild a) String
+
+type alias Blocks a =
+    List (Block a)
+
+
+-- TODO 在实际解析时通过 Dict 对同名属性去重
+type Attr a =
+    Attr a (AttrChild a)
+
+type alias AttrChild a =
+    List (String, (Attr a))
